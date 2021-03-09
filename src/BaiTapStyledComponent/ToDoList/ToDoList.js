c6cf63194c37f45332d8c0afb2e66997e3de7c37
tienpahm <phamtienacu@gmail.com>
2021-03-09 17:50:12 +1100

    set up theme

--- src/BaiTapStyledComponent/ToDoList/ToDoList.js
+++ src/BaiTapStyledComponent/ToDoList/ToDoList.js
@@ -1,7 +1,15 @@
 import React, {Component} from "react";
+import {Container} from "../Container/Container";
+import {ThemeProvider} from "styled-components";
+import {ToDoListDarkTheme} from "../../JSSDemo/Theme/ToDoListDarkTheme";
+import {ToDoListLightTheme} from "../../JSSDemo/Theme/ToDoListLightTheme";
 
 export default class ToDoList extends Component {
   render() {
-    return <div>123</div>;
+    return (
+      <ThemeProvider theme={ToDoListLightTheme}>
+        <Container>123</Container>
+      </ThemeProvider>
+    );
   }
 }
